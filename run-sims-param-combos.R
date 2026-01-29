12679ad7e496598e0e2adfeeb35c4218c24c512f
melliott432 <80354441+melliott432@users.noreply.github.com>
2026-01-29 10:12:35 -0800

    edits

--- run-sims-param-combos.R
+++ run-sims-param-combos.R
@@ -12,100 +12,103 @@ library(grid)
 library(ggplotify)
 library(beepr)
 
-source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_SimData.R", echo=TRUE)
-source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_Estimate.R", echo=TRUE)
-source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_GetPower.R", echo=TRUE)
+setwd("E:/UCSD/Thesis/Bayesian cRCT design/code")
 
-setwd("D:/UCSD/Thesis/Bayesian cRCT design/code/sims_param_combos")
+source("function_SimData.R", echo=TRUE)
+source("function_Estimate.R", echo=TRUE)
+source("function_GetPower.R", echo=TRUE)
+
+setwd("sims_param_combos")
 
 ### CONSTANT OPERTAING CHARACTERISTICS #########################################
 alpha = 0.05 # significance level
 n_arm = 2 # number of experimental arms
 sd_mult = 0.05 # standard deviation for outcome model
 n_village = 10 
-n_ind = 200
+n_ind = 50
 
 ### PARAMS TO VARY #############################################################
-pii = c(0.50, 0.75, 0.90)
-tau = c(0.25, 0.50, 0.75)
-delta = c(0.1, 0.2, 0.3)
+pii = seq(0.1, 0.95, by = 0.05)
+tau = seq(0.1, 0.95, by = 0.05)
+delta = seq(0.1, 0.95, by = 0.05)
 sim_params <- expand.grid(pii = pii, tau = tau, delta = delta)
 
-### SAVE RUN INFO ##############################################################
+### RUN INFO ##############################################################
 labels <- c("run_name", "follow_up_rate", "attrition_icc", "treatment_eff", "sim_follow_up_rate", "sim_attrition_icc", "sim_treatment_eff", "run.time")
-sim_info <- as.data.frame(matrix(data = NA, nrow = nrow(sim_sample_size), ncol = length(labels)))
+sim_info <- as.data.frame(matrix(data = NA, nrow = nrow(sim_params), ncol = length(labels)))
 colnames(sim_info) <- labels
 
+done = list.files(path = "power")
+
+sim_info = sim_params |> 
+  dplyr::mutate(
+    run_name <- paste("2026-01-28", pii*100, tau*100, delta*100, "n", n_village, "m", n_ind, sep = "_")
+  ) |> 
+  filter(
+  #  !(FILE %in% done)
+  #) 
+
+#### Only change input above this line ######################################### 
+################################################################################
 ################################################################################
 
 # Iterate through combos
 for(i in 1:nrow(sim_params)){
   
-  start.time <- Sys.time() # track run time
+  # Check if combo has been run
+  if(is.na(sim_info$run_name[i])){
     
-  ### Set Parameters ###
-  
-  pii <- sim_params[i, "pii"] # follow-up rate
-  tau <- sim_params[i, "tau"] # attrition ICC
-  delta <- sim_params[i, "delta"] # treatment effect
+    start.time <- Sys.time() # track run time
     
-  parms <- list("n_village" = n_village, 
+    ############################################################################
+    #### SIMULATE DATA #########################################################
+    
+    pii <- sim_params$pii[i]
+    tau <- sim_params$tau[i]
+    delta <- sim_params$delta[i]
+    
+    parms <- list("n_village" = n_village, 
                   "n_ind" = n_ind, 
                   "n_arm" = n_arm, 
                   "pii" = pii, 
                   "tau" = tau, 
                   "delta" = delta, 
-                  "sd_mult" = sd_mult)
+                  "sd_mult" = sd_mult,
+                  "alpha" = alpha)
     
-
-  ### Run simulation and estimation ###
-  
-  run_name <- paste(Sys.Date(), pii, tau, delta, sep = "_") 
-
-  simdata <- SimData(run_name = run_name, parms = parms)
+    simdata <- SimData(run_name = run_name, parms = parms)
     
-  estimates <- SimEstimate(run_name = run_name, simdata = simdata)
+    estimates <- SimEstimate(run_name = run_name, simdata = simdata)
     
-  power <- GetPower(run_name = run_name, simdata = simdata, estimates = estimates)
-  
-  png(filename = paste("plots/power/", run_name, "_power", ".png", sep = ""))
-  plot(density(power), main = paste("n = ", n_village, ", m = ", n_ind, sep = ""), xlab = "Power")
-  dev.off()
-  
-  ### Check Sim Data ### 
-  
-  pi.sim <- mean(simdata$followup_final_indic)
+    power <- GetPower(run_name = run_name, simdata = simdata, estimates = estimates, parms = parms)
     
-  pr = simdata |>
-    group_by(village_id) |>
-    summarize(prop = sum(followup_final_indic)/length(followup_final_indic))
     
-  tau.sim <- var(pr$prop)/(pi.sim*(1-pi.sim))
+    # Check pi
+    pi.sim <- mean(simdata$followup_final_indic)
     
-  a.sim <- (pi.sim*(1-tau.sim))/tau.sim
-  b.sim <- (1/tau.sim) - 1 - ((pi.sim*(1-tau.sim))/tau.sim)
+    # Check tau
+    pr = simdata |>
+      group_by(village_id) |>
+      summarize(prop = sum(followup_final_indic)/length(followup_final_indic))
     
-  p1 <- mean(simdata$adherence_final_indic[which(simdata$arm_id==1)])
-  p2 <- mean(simdata$adherence_final_indic[which(simdata$arm_id==2)])
-  delta.sim <- p2 - p1
-  
-
-  # Get run time
-  end.time <- Sys.time()
-  run.time <- end.time - start.time
+    tau.sim <- var(pr$prop)/(pi.sim*(1-pi.sim))
     
-  # Save info about this run
-  sim_info[i,] <- c(run_name, pii, tau, delta, round(c(pi.sim, tau.sim, delta.sim, run.time),3))
-
+    a.sim <- (pi.sim*(1-tau.sim))/tau.sim
+    b.sim <- (1/tau.sim) - 1 - ((pi.sim*(1-tau.sim))/tau.sim)
+    
+    # Check treatment effect
+    p1 <- mean(simdata$adherence_final_indic[which(simdata$arm_id==1)])
+    p2 <- mean(simdata$adherence_final_indic[which(simdata$arm_id==2)])
+    delta.sim <- p2 - p1
+    
+    end.time <- Sys.time()
+    
+    run.time <- end.time - start.time
+    
+    sim_info[i,] <- c(run_name, pii, tau, delta, round(c(pi.sim, tau.sim, delta.sim, run.time),3))
+    
+  }
 }
 
-save(sim_info, file = "info.RData")
-
-#################################################################################
-
-
-
-
-
-
-
+save(sim_info, file = paste(Sys.Date(), "_n", n_village, "_m", n_ind, "_", "info.RData", sep = ""))
+ 
--- run-sims-sample-size.R
+++ run-sims-sample-size.R
@@ -12,8 +12,9 @@ library(grid)
 library(ggplotify)
 library(beepr)
 
-source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_SimData.R", echo=TRUE)
-source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_Estimate.R", echo=TRUE)
+source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_SimData.R")
+source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_Estimate.R")
+source("D:/UCSD/Thesis/Bayesian cRCT design/code/function_GetPower.R")
 
 setwd("D:/UCSD/Thesis/Bayesian cRCT design/code/sims_sample_size")
 
@@ -26,11 +27,12 @@ delta = 0.2 # treatment effect
 sd_mult = 0.01 # standard deviation for outcome model
 
 # Sample size combinations to try
-n_clusters = seq(5, 200, by = 20)
-n_clusters <- ifelse(n_clusters %% 2 == 0, n_clusters, n_clusters+1)
-sim_sample_size <- expand.grid(n_clusters = n_clusters, cluster_size = round(seq(5, 160, length.out = 10)))
-sim_sample_size$total_samp_size <- sim_sample_size$n_clusters * sim_sample_size$cluster_size
-sim_sample_size <- sim_sample_size |> filter(total_samp_size < 5000) 
+n_clusters = c(6, 10, 16, 20)
+cluster_size = c(25, 50, 100, 200)
+#n_clusters <- ifelse(n_clusters %% 2 == 0, n_clusters, n_clusters+1)
+sim_sample_size <- expand.grid(n_clusters = n_clusters, cluster_size = cluster_size)
+#sim_sample_size$total_samp_size <- sim_sample_size$n_clusters * sim_sample_size$cluster_size
+#sim_sample_size <- sim_sample_size |> filter(total_samp_size < 5000) 
 
 # Data frame to save info about each simulation
 labels <- c("run_name", "n_village", "n_ind", "n_total", "sim_follow_up_rate", "sim_attrition_icc", "sim_treatment_eff", "run.time")
@@ -56,12 +58,23 @@ for(i in 1:nrow(sim_sample_size)){
     n_ind <- sim_sample_size$cluster_size[i]
     n_ind_total <- n_village*n_ind
     
-    run_name <- paste(Sys.Date(), n_village, n_ind, sep = "_") # label for this run
+    run_name <- paste(Sys.Date(), "_", n_village,"_", n_ind, sep = "") # label for this run
     
-    # Call data sim function
-    simdata <- SimData(n_village = n_village, n_ind = n_ind, n_arm = n_arm, pii=pii, tau = tau, delta = delta, sd_mult = sd_mult)
-    filename <- paste("datasets/",paste(run_name, "RData", sep = "."), sep="")
-    save(simdata, file = filename)
+    parms <- list("n_village" = n_village, 
+                  "n_ind" = n_ind, 
+                  "n_arm" = n_arm, 
+                  "pii" = pii, 
+                  "tau" = tau, 
+                  "delta" = delta, 
+                  "sd_mult" = sd_mult,
+                  "alpha" = alpha)
+    
+    simdata <- SimData(run_name = run_name, parms = parms)
+    
+    estimates <- SimEstimate(run_name = run_name, simdata = simdata)
+    
+    power <- GetPower(run_name = run_name, simdata = simdata, estimates = estimates, parms = parms)
+    
     
     # Check pi
     pi.sim <- mean(simdata$followup_final_indic)
@@ -80,44 +93,6 @@ for(i in 1:nrow(sim_sample_size)){
     p1 <- mean(simdata$adherence_final_indic[which(simdata$arm_id==1)])
     p2 <- mean(simdata$adherence_final_indic[which(simdata$arm_id==2)])
     delta.sim <- p2 - p1
-  
-    ############################################################################
-    #### ESTIMATE #######################################################
-    
-    estimates <- Sim_Estimate(simdata = simdata)
-    
-    ############################################################################
-    #### CALCULATE POWER #######################################################
-    
-    pr = simdata |>
-      group_by(village_id) |>
-      summarize(prop = sum(adherence_base_indic)/length(adherence_base_indic))
-    var.p = var(pr$prop)
-    exp.p = mean(pr$prop)
-    rho_y = var.p / (exp.p*(1-exp.p))
-    
-    p1 = mean(simdata$adherence_base_indic)
-    p2 = delta + p1
-    
-    # Sample parameters from posterior distribution
-    B = 1000
-    a.B <- sample(estimates[,"a"], B)
-    b.B <- sample(estimates[,"b"], B)
-    delta.B <- sample(estimates[,"delta"],B)
-    tau.B <- 1/(a.B+b.B+1)
-    pii.B <- a.B/(a.B+b.B)
-    
-    G = 1 + (n_ind*pii.B - 1)*rho_y + (1-pii.B)*(1 + (n_ind-1)*tau.B)*rho_y
-    
-    term = n_village*n_ind*delta.B^2 *(pii.B/G) *(p1*(1-p1) + (delta.B+p1)*(1-(delta.B+p1)))^-1
-    
-    power <- pnorm(sqrt(term) - qnorm(1-alpha/2))
-    
-
-    # Power
-    png(filename = paste("plots/power/", run_name, "_power", ".png", sep = ""))
-    plot(density(power), main = paste("n = ", n_village, ", m = ", n_ind, sep = ""), xlab = "Power")
-    dev.off()
     
     end.time <- Sys.time()
     
