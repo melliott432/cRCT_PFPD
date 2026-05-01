e9f5df4d9235be4810296c75a37fed52a45aac10
melliott432 <80354441+melliott432@users.noreply.github.com>
2026-05-01 10:41:51 -0700

    Update make_info_grids.R
    added comments

--- make_info_grids.R
+++ make_info_grids.R
@@ -1,5 +1,5 @@
 
-nreps = 5
+nreps = 5 # number of simulation replications to run
 
 alpha = 0.05 # significance level
 n_arm = 2 # number of experimental arms
@@ -8,14 +8,14 @@ sd_mult = 0.05 # standard deviation for outcome model
 ################################################################################
 ### SAMPLE SIZE COMBINATIONS ###################################################
 
-n_village = c(6, 10, 16, 20)
-n_ind = c(25, 50, 100, 200)
+n_village = c(6, 10, 16, 20) # number of clusters
+n_ind = c(25, 50, 100, 200) # cluster size 
 
 pii = 0.42 # follow-up rate
 tau = 0.06 # attrition ICC
 delta = 0.2 # effect size
 
-ss <- expand.grid("n_village" = n_village, "n_ind" = n_ind, rep = 1:nreps)
+ss <- expand.grid("n_village" = n_village, "n_ind" = n_ind, rep = 1:nreps) # combinations of cluster size and number of clusters
 
 siminfo <- data.frame(ss,
                       "n_arm" = n_arm, 
@@ -32,14 +32,14 @@ save(siminfo, file = "D:/UCSD/Thesis/Bayesian cRCT design/results/simulations/sa
 ################################################################################
 ### PI/TAU/DELTA COMBINATIONS ##################################################
 
-n_village = 10 
-n_ind = 20
+n_village = 10 # hold number of clusters constant
+n_ind = 20 # hold cluster size constant
 
-pii = seq(0.1, 0.95, by = 0.05)
-tau = seq(0.1, 0.95, by = 0.05)
-delta = seq(0.1, 0.70, by = 0.05)
+pii = seq(0.1, 0.95, by = 0.05) # follow-up rate
+tau = seq(0.1, 0.95, by = 0.05) # attrition ICC
+delta = seq(0.1, 0.70, by = 0.05) # treatment effect
 
-siminfo <- expand.grid("pii" = pii, "tau" = tau, "delta" = delta, rep = 1:nreps)
+siminfo <- expand.grid("pii" = pii, "tau" = tau, "delta" = delta, rep = 1:nreps) # all pi/tau/delta combinations
 
 siminfo <- data.frame("n_village" = n_village, 
                       "n_ind" = n_ind, 
